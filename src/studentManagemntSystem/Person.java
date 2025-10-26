17aa6160b5e21666486224f9463545c749a63b4f
Aya-Gebril <aya2gebril@gmail.com>
2025-10-26 15:08:33 +0300

    Person branch3

--- src/studentManagemntSystem/Person.java
+++ src/studentManagemntSystem/Person.java
@@ -5,11 +5,34 @@ public class Person {
     protected int age;
     protected String gender;
 
-    public Person (String Fullname, int age, String gender){
+    public Person(String Fullname, int age, String gender) {
         this.Fullname = Fullname;
         this.age = age;
         this.gender = gender;
     }
 
+    public String getFullname() {
+        return Fullname;
+    }
+
+    public void setFullname(String fullname) {
+        Fullname = fullname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
 }

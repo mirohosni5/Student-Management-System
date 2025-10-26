5c13fd91e5a81784c886abe3104ef56ec0f32a36
mayar <mirohosni5@gmail.com>
2025-10-27 00:04:04 +0300

    auto: MAYAR 2025-10-27T00:04:04.2883455+03:00

--- src/studentManagemntSystem/DataHandler.java
+++ src/studentManagemntSystem/DataHandler.java
@@ -2,13 +2,24 @@ package studentManagemntSystem;
 import java.io.*;
 import java.util.*;
 public abstract class DataHandler {
-public final ArrayList<Object> students = new ArrayList<>();
-public abstract Object parseli(String line);
-public abstract String toline(Object obj);
-public abstract String keyoff(Object obj);
+    public final ArrayList<Object> students = new ArrayList<>();
+
+    public abstract Object parseli(String line);
+
+    public abstract String toline(Object obj);
+
+    public abstract String keyoff(Object obj);
+    public void saveToFile(){
+        try (PrintWriter pw = new PrintWriter(new FileWriter(filename))) {
+            for (Object obj : records) {
+                recordInterfaces rec = (recordInterfaces) obj;
+                pw.println(rec.lineRepresentation());
+            }
+            System.out.println(filename + " is saved");
+        } catch (IOException e) {
+            System.out.println(filename + " couldn't save");
+        }
+    }
 
 
-    public abstract void saveToFile(String filename);
-
-    public abstract void loadFromFile(String filename);
 }

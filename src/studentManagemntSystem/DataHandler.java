64189a7a74dd9a219e32ac9cbf013175db0839d3
mayar <mirohosni5@gmail.com>
2025-10-27 00:00:19 +0300

    auto: MAYAR 2025-10-27T00:00:19.3969375+03:00

--- src/studentManagemntSystem/DataHandler.java
+++ src/studentManagemntSystem/DataHandler.java
@@ -2,7 +2,7 @@ package studentManagemntSystem;
 import java.io.*;
 import java.util.*;
 public abstract class DataHandler {
-public final
+public final A
     public abstract void saveToFile(String filename);
 
     public abstract void loadFromFile(String filename);

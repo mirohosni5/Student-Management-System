73966447ab35bf1616e75a13814089cebc82b8eb
mayar <mirohosni5@gmail.com>
2025-10-27 00:32:29 +0300

    auto: MAYAR 2025-10-27T00:32:29.0242608+03:00

--- src/studentManagemntSystem/DataHandler.java
+++ src/studentManagemntSystem/DataHandler.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 public abstract class DataHandler {
     public final ArrayList<Object> students = new ArrayList<>();
-public abstract Object createrecord
+public abstract Object creator
     public abstract Object parseli(String line);
 
     public abstract String toline(Object obj);

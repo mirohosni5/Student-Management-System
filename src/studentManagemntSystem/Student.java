0e561d2dd9817c8a3c2087492d0862443d6a5aeb
Aya-Gebril <aya2gebril@gmail.com>
2025-10-26 16:30:01 +0300

    Student branch

--- src/studentManagemntSystem/Student.java
+++ src/studentManagemntSystem/Student.java
@@ -2,14 +2,26 @@ package studentManagemntSystem;
 
 public class Student extends Person {
     private int ID;
-    private  String Department;
+    private String Department;
     private double GPA;
 
-    public Student(int ID, String name, int age, String gender, String Department, double GPA){
+    public Student(int ID, String name, int age, String gender, String Department, double GPA) {
         super(name, age, gender);
         this.ID = ID;
         this.Department = Department;
-        this.GPA = GPA;
-    }
+        this.GPA = GPA;}
+
+    public int getID() {return ID;}
+
+    public void setID(int ID) {this.ID = ID;}
+
+    public double getGPA() {return GPA;}
+
+    public void setGPA(double GPA) {this.GPA = GPA;}
+
+    public String getDepartment() {return Department;}
+
+    public void setDepartment(String department) {Department = department;}
+
 
 }

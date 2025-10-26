3ec9111c5bb5395dec111ff4f51bedcdf3b11b9a
Aya-Gebril <aya2gebril@gmail.com>
2025-10-26 16:27:26 +0300

    Student branch is a child of Person branch

--- src/studentManagemntSystem/Student.java
+++ src/studentManagemntSystem/Student.java
@@ -1,4 +1,15 @@
 package studentManagemntSystem;
 
-public class Student {
+public class Student extends Person {
+    private int ID;
+    private  String Department;
+    private double GPA;
+
+    public Student(int ID, String name, int age, String gender, String Department, double GPA){
+        super(name, age, gender);
+        this.ID = ID;
+        this.Department = Department;
+        this.GPA = GPA;
+    }
+
 }

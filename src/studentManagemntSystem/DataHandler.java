57082f9a4c262c32238d46ca1f821efd2c6d5284
mayar <mirohosni5@gmail.com>
2025-10-27 00:48:59 +0300

    auto: MAYAR 2025-10-27T00:48:59.6466772+03:00

--- src/studentManagemntSystem/DataHandler.java
+++ src/studentManagemntSystem/DataHandler.java
@@ -3,11 +3,7 @@ package studentManagemntSystem;
 import java.io.*;
 import java.util.*;
 
-public abstract class DataHandler extends Person {
-    public DataHandler(String Fullname, int age, String gender) {
-        super(Fullname, age, gender);
-    }
-
+public abstract class DataHandler {
 
     protected final ArrayList<Object> students = new ArrayList<>();
 

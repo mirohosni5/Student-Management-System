220b06c92e4ee93b292a4a1ec9ef2fcb445bf5ae
mayar <mirohosni5@gmail.com>
2025-10-27 00:13:30 +0300

    auto: MAYAR 2025-10-27T00:13:29.9459350+03:00

--- src/studentManagemntSystem/DataHandler.java
+++ src/studentManagemntSystem/DataHandler.java
@@ -54,7 +54,7 @@ public abstract class DataHandler {
     }
     public void add(Object obj) {
         if (obj == null) return;
-        String key 
+        String key =keyoff()
         if (keyoff(obj) == null) return;
         records.add(obj);
     }

b3e4fbd306045bfc24108366de9697cd81304476
mayar <mirohosni5@gmail.com>
2025-10-27 21:27:35 +0300

    auto: MAYAR 2025-10-27T21:27:34.9907449+03:00

--- src/studentManagemntSystem/Admin.java
+++ src/studentManagemntSystem/Admin.java
@@ -5,5 +5,8 @@ public class Admin {
     public String username;
     public String password;
 
-public Admin(String username, String password)
+public Admin(String username, String password){
+    this.username=username;
+    this.password=password;
+}
 }

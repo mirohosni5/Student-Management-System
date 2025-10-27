ed6c6d7bb7a4795f091eb7c6691de1b24a5b9d6a
mayar <mirohosni5@gmail.com>
2025-10-27 20:51:32 +0300

    auto: MAYAR 2025-10-27T20:51:32.8106283+03:00

--- src/studentManagemntSystem/StudentDatabase.java
+++ src/studentManagemntSystem/StudentDatabase.java
@@ -24,7 +24,7 @@ public class StudentDatabase extends DataHandler{
 
                 Student s = new Student(id, name, age, gender, dept, gpa);
 
-                
+
                 if (id >= nextId) nextId = id + 1;
 
                 return s; // parent stores it as Object

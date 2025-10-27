aff4ba33b87968b08a947d5bb0034733827ed81a
Aya-Gebril <aya2gebril@gmail.com>
2025-10-27 22:50:08 +0300

    StudentAccount

--- src/backEnd/StudentAccount.java
+++ src/backEnd/StudentAccount.java
@@ -4,4 +4,12 @@ public class StudentAccount {
     private int studentId;
     private String password;
 
+    public StudentAccount(int studentId, String password) {
+        this.studentId = studentId;
+        this.password = password;
+    }
+
+    public int getStudentId() { return studentId; }
+    public String getPassword() { return password;}
+
 }

ea65c490aeff21a12598117e53648c9ed0701063
mayar <mirohosni5@gmail.com>
2025-10-27 23:24:32 +0300

    auto: MAYAR 2025-10-27T23:24:32.4427046+03:00

--- src/backEnd/StudentDatabase.java
+++ src/backEnd/StudentDatabase.java
@@ -165,10 +165,7 @@ public class StudentDatabase extends DataHandler{
         double gpa = s.getGPA();
         if (Double.isNaN(gpa)) //NaN means not a number
             return false;
-        if (gpa < 0.0 || gpa > 4.0)
-            return false;
-
-        return true;
+        return !(gpa < 0.0) && !(gpa > 4.0);
     }
 
 }

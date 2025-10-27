1164073de180f62dc431ae0c663386b49fbb6d48
mayar <mirohosni5@gmail.com>
2025-10-27 22:08:30 +0300

    auto: MAYAR 2025-10-27T22:08:30.3800909+03:00

--- src/backEnd/loginHandling.java
+++ src/backEnd/loginHandling.java
@@ -2,7 +2,7 @@ package backEnd;
 
 public class loginHandling {
     public final adminData admins=new adminData();
-    public final StudentAccountdata studentaccodata=newStudent
+    public final StudentAccountdata studentaccodata=new Student
     public Admin now;
 
     //add here aya
